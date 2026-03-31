f10a44654ec0a0ad14d2bb69586bfd9564c08f28
AdelGann <imadelg14@gmail.com>
2026-03-31 12:30:57 -0400

    feat: initial architecture

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Amazing Router React Plugin - File-System Based Routing Plugin for react
+ * @author Adel Gannem a.k.a im_adelg
+ */
+
+// plugins
+export * from "@amazing_router/react/plugins/vite";
+export * from "@amazing_router/react/plugins/webpack";
+
+// hooks
+export * from "@amazing_router/react/hooks/index";
+
+// utils
+export * from "@amazing_router/react/utils/index";
+
+// types
+export * from "@amazing_router/react/types/index";
